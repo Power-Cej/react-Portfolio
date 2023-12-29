8858241b6d12c75d72ab570b052e6cce8dea6709
no_Name <caballeroceejay5@gmail.com>
2023-12-29 17:51:15 +0800

    contact page responsiveness

--- src/Pages/CONTACT/Contact.jsx
+++ src/Pages/CONTACT/Contact.jsx
@@ -6,6 +6,11 @@ import facebook from '../../Components/ContactImages/facebook.png';
 import telegram from '../../Components/ContactImages/telegram.png';
 
 const Contact = () => {
+
+  const handleDownload = () => {
+    window.location.href = 
+    'https://export-download.canva.com/Oa1t4/DAFyf1Oa1t4/278/0-6725413942860789387.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20231228%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231228T140907Z&X-Amz-Expires=73140&X-Amz-Signature=c03f510c7cf62983bb33929dd90450330c69d3a7dfb5178eb74c6d094b666dca&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27SysDev_Caballero%2520Ceejay%2520B.pdf&response-expires=Fri%2C%2029%20Dec%202023%2010%3A28%3A07%20GMT';
+  }
   return (
     <>
     <div className='contactContent'>
@@ -30,7 +35,7 @@ const Contact = () => {
           <img src={telegram} alt='TELEGRAm'/>
           </a>
 
-          <button>Download CV</button>
+          <button onClick={handleDownload}>Download CV</button>
         </div>
       </div>
     </div>
