42a7e647d97b232a97b9aee828396cedd8eeaabb
no_Name <caballeroceejay5@gmail.com>
2024-01-03 20:55:31 +0800

    adjust responsiveness

--- src/Pages/CONTACT/Contact.jsx
+++ src/Pages/CONTACT/Contact.jsx
@@ -1,82 +1,112 @@
-import React from 'react';
-import './contact.css';
+import React from "react";
+import "./contact.css";
 
-import github from '../../Components/ContactImages/github.png';
-import facebook from '../../Components/ContactImages/facebook.png';
-import telegram from '../../Components/ContactImages/telegram.png';
-import pdfFile from './CVFile/cv.pdf';
+import github from "../../Components/ContactImages/github.png";
+import facebook from "../../Components/ContactImages/facebook.png";
+import telegram from "../../Components/ContactImages/telegram.png";
+import pdfFile from "./CVFile/cv.pdf";
 
 const Contact = () => {
-
   const handleDownload = () => {
     // Create a link element
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = pdfFile;
-    link.download = 'cv.pdf';
+    link.download = "cv.pdf";
     link.click(); // Simulate a click on the link
   };
 
-
   return (
     <>
-    <div className='contactContent'>
-    <div className='contact-left'>
-      <div className='contact-details'>
-        <h3>CONTACT</h3>
-        <p>
-          Feel free to contact me by submitting the form below and I will get back to you
-          as soon as possible, or you can just find me here:
-        </p>
+      <div className="contactContent">
+        <div className="contact-left">
+          <div className="contact-details">
+            <h3>CONTACT</h3>
+            <p>
+              Feel free to contact me by submitting the form below and I will
+              get back to you as soon as possible, or you can just find me here:
+            </p>
 
-        <div className='contact-link'>
-        <a className='anchor' href="https://github.com/Power-Cej" target="_blank">
-          <img src={github} alt='GITHUB'/>
-          </a>
+            <div className="contact-link">
+              <a
+                className="anchor"
+                href="https://github.com/Power-Cej"
+                target="_blank"
+              >
+                <img src={github} alt="GITHUB" />
+              </a>
 
-          <a className='anchor' href="https://www.facebook.com/caballero880" target="_blank">
-          <img src={facebook} alt='FACEBOOK'/>
-          </a>
+              <a
+                className="anchor"
+                href="https://www.facebook.com/caballero880"
+                target="_blank"
+              >
+                <img src={facebook} alt="FACEBOOK" />
+              </a>
 
-          <a className='anchor' href="https://web.telegram.org/k/#@cejCaballero" target="_blank">
-          <img src={telegram} alt='TELEGRAm'/>
-          </a>
+              <a
+                className="anchor"
+                href="https://web.telegram.org/k/#@cejCaballero"
+                target="_blank"
+              >
+                <img src={telegram} alt="TELEGRAm" />
+              </a>
 
-          <button onClick={handleDownload}>Download CV</button>
+              <button onClick={handleDownload}>Download CV</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="contact-right">
+          <div className="contat-email">
+            <form
+              action="mailto:caballeroceejay5@gmail.com?subject=Contact%20Form"
+              method="post"
+              encType="text/plain"
+            >
+              <div className="contact-field">
+                <label className="contact-label">Name</label>
+                <input
+                  required
+                  placeholder="Enter Your Name"
+                  type="text"
+                  className="contact-input"
+                  name="name"
+                  id="name"
+                />
+              </div>
+
+              <div className="contact-field">
+                <label className="contact-label">Email</label>
+                <input
+                  required
+                  placeholder="Enter Your Email"
+                  type="email"
+                  className="contact-input"
+                  name="email"
+                  id="email"
+                />
+              </div>
+
+              <div className="contact-field">
+                <label className="contact-label">Message</label>
+                <textarea
+                  required
+                  placeholder="Enter Your Message"
+                  className="contact-input"
+                  id="message"
+                  rows="10"
+                  name="message"
+                ></textarea>
+              </div>
+              <button type="submit" id="button">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div className='contact-right'>
-      <div className='contat-email'>
-      <form action="mailto:caballeroceejay5@gmail.com?subject=Contact%20Form" method="post" encType="text/plain">
-            <div className="contact-field">
-                <label className="contact-label">
-                    Name
-                </label>
-                <input required placeholder="Enter Your Name" type="text" className="contact-input" name="name" id="name"/>
-            </div>
-    
-            <div className="contact-field">
-                <label className="contact-label">
-                    Email
-                </label>
-                <input required placeholder="Enter Your Email" type="email" className="contact-input" name="email" id="email"/>
-            </div>
-    
-            <div className="contact-field">
-                <label className="contact-label">
-                    Message
-                </label>
-                <textarea required placeholder="Enter Your Message" className="contact-input" id="message" rows="10" name="message"></textarea>
-            </div>
-            <button type="submit" id="button">Submit</button>
-        </form>
-      </div>
-</div>
-
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Pages/WORKS & CERTIFICATE/Works.jsx
+++ src/Pages/WORKS & CERTIFICATE/Works.jsx
@@ -1,54 +1,49 @@
-import './works.css'
-import Projects from './Projects';
-import Certificates from './Certificate';
+import "./works.css";
+import Projects from "./Projects";
+import Certificates from "./Certificate";
 
-import { projectData, certificateData } from '../../Components/Data/projectData';
+import {
+  projectData,
+  certificateData,
+} from "../../Components/Data/projectData";
 
 const Works = () => {
-
-  
-
   return (
     <>
-    <div className='workContent'>
-    <h3>WORKS</h3>
-    <div className='projectList'>
-      {projectData.map((project, index) => {
-        return (
-            <Projects
-          key={index}
-          payroll={project.image[0]}
-          pageLink={project.pageLink}
-          title={project.title}
-          paragraph={project.paragraph}
-          index={index}
-        />
-        );
-        
-})}
-</div>
+      <div className="workContent">
+        <h3>WORKS</h3>
+        <div className="projectList">
+          {projectData.map((project, index) => {
+            return (
+              <Projects
+                key={index}
+                payroll={project.image[0]}
+                pageLink={project.pageLink}
+                title={project.title}
+                paragraph={project.paragraph}
+                index={index}
+              />
+            );
+          })}
+        </div>
 
-
-<h3>Certificate</h3>
-    <div className='projectList'>
-      {certificateData.map((certificate, index) => {
-        return (
-            <Certificates
-          key={index}
-          payroll={certificate.image}
-          pageLink={certificate.pageLink}
-          title={certificate.title}
-          paragraph={certificate.paragraph}
-        />
-        );
-        
-})}
-</div>
-
-
-    </div>
+        <h3>CERTIFICATE</h3>
+        <div className="projectList">
+          {certificateData.map((certificate, index) => {
+            return (
+              <Certificates
+                key={index}
+                payroll={certificate.image}
+                pageLink={certificate.pageLink}
+                title={certificate.title}
+                paragraph={certificate.paragraph}
+              />
+            );
+          })}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
