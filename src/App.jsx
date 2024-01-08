16da93e63cdc5829e46634424a29de03e33a3f0e
no_Name <caballeroceejay5@gmail.com>
2024-01-08 17:32:56 +0800

    adjust mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,40 @@
-import {useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// PAGES 
-import HomePage from './Pages/HOME/HomePage'
-import Works from './Pages/WORKS & CERTIFICATE/Works'
-import WorkPage from './Pages/WORKS & CERTIFICATE/WORK_GALLERY/WorkPage'
-import LoadingScreen from './Pages/LOADING/LoadingScreen'
+// PAGES
+import HomePage from "./Pages/HOME/HomePage";
+import Works from "./Pages/WORKS & CERTIFICATE/Works";
+import WorkPage from "./Pages/WORKS & CERTIFICATE/WORK_GALLERY/WorkPage";
+import LoadingScreen from "./Pages/LOADING/LoadingScreen";
 
 function App() {
-
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 2000);
+    }, 2500);
 
     return () => clearTimeout(timer);
-
-  })
+  });
 
   return (
     // <MyContextProvider>
-      <BrowserRouter>
-      {
-        isLoading ? 
-        (
-          <LoadingScreen />
-        )
-        :
-        (
+    <BrowserRouter>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/works' element={<Works />} />
-          <Route path='/projects' element={<WorkPage />} />
-          <Route path='/load' element={<LoadingScreen />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/projects" element={<WorkPage />} />
+          <Route path="/load" element={<LoadingScreen />} />
         </Routes>
-        )
-      }
-        
-      </BrowserRouter>
-
-  )
+      )}
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
